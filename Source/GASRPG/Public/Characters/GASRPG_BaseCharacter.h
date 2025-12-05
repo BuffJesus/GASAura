4ee0a1b6c3005845098ab9efe068920d6050c201
BuffJesus <69093219+BuffJesus@users.noreply.github.com>
2025-12-04 21:07:39 -0700

    Add base character class, set default/editor startup map, and update animations/montages with new asset structure.

---
+++ Source/GASRPG/Public/Characters/GASRPG_BaseCharacter.h
@@ -0,0 +1,15 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/Character.h"
+#include "GASRPG_BaseCharacter.generated.h"
+
+UCLASS()
+class GASRPG_API AGASRPG_BaseCharacter : public ACharacter
+{
+	GENERATED_BODY()
+
+
+};
