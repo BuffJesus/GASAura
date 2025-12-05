f012113c9320a4eb603cbe90401ca305757e71a4
BuffJesus <69093219+BuffJesus@users.noreply.github.com>
2025-12-04 22:26:39 -0700

    Add `GASRPG_PlayerController` class with input handling and movement logic, and update module dependencies for gameplay systems integration

---
+++ Source/GASRPG/Private/Characters/Player/GASRPG_PlayerController.cpp
@@ -0,0 +1,54 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "Characters/Player/GASRPG_PlayerController.h"
+#include "EnhancedInputComponent.h"
+#include "EnhancedInputSubsystems.h"
+#include "Characters/Player/GASRPG_PlayerCharacter.h"
+
+AGASRPG_PlayerController::AGASRPG_PlayerController()
+{
+	bReplicates = true;
+}
+
+void AGASRPG_PlayerController::BeginPlay()
+{
+	Super::BeginPlay();
+	check(GASRPGContext);
+	
+	UEnhancedInputLocalPlayerSubsystem* Subsystem { ULocalPlayer::GetSubsystem<UEnhancedInputLocalPlayerSubsystem>(GetLocalPlayer()) };
+	check(Subsystem);
+	Subsystem->AddMappingContext(GASRPGContext, 0);
+	
+	bShowMouseCursor = true;
+	DefaultMouseCursor = EMouseCursor::Default;
+	
+	FInputModeGameAndUI InputMode;
+	InputMode.SetLockMouseToViewportBehavior(EMouseLockMode::DoNotLock);
+	InputMode.SetHideCursorDuringCapture(false);
+	SetInputMode(InputMode);
+}
+
+void AGASRPG_PlayerController::SetupInputComponent()
+{
+	Super::SetupInputComponent();
+	
+	UEnhancedInputComponent* EnhancedInputComponent { CastChecked<UEnhancedInputComponent>(InputComponent) };
+	EnhancedInputComponent->BindAction(MoveAction, ETriggerEvent::Triggered, this, &AGASRPG_PlayerController::Move);
+}
+
+void AGASRPG_PlayerController::Move(const FInputActionValue& InputActionValue)
+{
+	const FVector2D InputAxisVector { InputActionValue.Get<FVector2D>() };
+	const FRotator Rotation { GetControlRotation() };
+	const FRotator YawRotation { 0., Rotation.Yaw, 0.f };
+	
+	const FVector ForwardDirection { FRotationMatrix(YawRotation).GetUnitAxis(EAxis::X) };
+	const FVector RightDirection { FRotationMatrix(YawRotation).GetUnitAxis(EAxis::Y) };
+	
+	if (APawn* ControlledPawn { GetPawn<APawn>() })
+	{
+		ControlledPawn->AddMovementInput(ForwardDirection, InputAxisVector.Y);
+		ControlledPawn->AddMovementInput(RightDirection, InputAxisVector.X);
+	}
+}
---
+++ Source/GASRPG/Public/Characters/Player/GASRPG_PlayerController.h
@@ -0,0 +1,35 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/PlayerController.h"
+#include "GASRPG_PlayerController.generated.h"
+
+struct FInputActionValue;
+class UInputMappingContext;
+class UInputAction;
+/**
+ * 
+ */
+UCLASS()
+class GASRPG_API AGASRPG_PlayerController : public APlayerController
+{
+	GENERATED_BODY()
+	
+public:
+	AGASRPG_PlayerController();
+	
+protected:
+	virtual void BeginPlay() override;	
+	virtual void SetupInputComponent() override;
+	
+private:
+	UPROPERTY(EditAnywhere, Category = "GASRPG|Input")
+	TObjectPtr<UInputMappingContext> GASRPGContext;
+	
+	UPROPERTY(EditAnywhere, Category = "GASRPG|Input")
+	TObjectPtr<UInputAction> MoveAction;
+	
+	void Move(const FInputActionValue& InputActionValue);
+};
