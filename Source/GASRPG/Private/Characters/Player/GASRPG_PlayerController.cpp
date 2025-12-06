53671f4fe358cf9e5c9e2c9b73dd5c61b0a0eef2
BuffJesus <69093219+BuffJesus@users.noreply.github.com>
2025-12-05 18:09:43 -0700

    Update DefaultEngine.ini to set global default game mode and simplify movement input handling in GASRPG_PlayerController

--- Source/GASRPG/Private/Characters/Player/GASRPG_PlayerController.cpp
+++ Source/GASRPG/Private/Characters/Player/GASRPG_PlayerController.cpp
@@ -40,15 +40,11 @@ void AGASRPG_PlayerController::SetupInputComponent()
 void AGASRPG_PlayerController::Move(const FInputActionValue& InputActionValue)
 {
 	const FVector2D InputAxisVector { InputActionValue.Get<FVector2D>() };
-	const FRotator Rotation { GetControlRotation() };
-	const FRotator YawRotation { 0., Rotation.Yaw, 0.f };
-	
-	const FVector ForwardDirection { FRotationMatrix(YawRotation).GetUnitAxis(EAxis::X) };
-	const FVector RightDirection { FRotationMatrix(YawRotation).GetUnitAxis(EAxis::Y) };
 	
 	if (APawn* ControlledPawn { GetPawn<APawn>() })
 	{
-		ControlledPawn->AddMovementInput(ForwardDirection, InputAxisVector.Y);
-		ControlledPawn->AddMovementInput(RightDirection, InputAxisVector.X);
+		ControlledPawn->AddMovementInput(FVector::ForwardVector, InputAxisVector.Y);
+		ControlledPawn->AddMovementInput(FVector::RightVector, InputAxisVector.X);
 	}
+	
 }
