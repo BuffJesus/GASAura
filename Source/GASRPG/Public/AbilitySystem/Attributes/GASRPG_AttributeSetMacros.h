ee7eafcbfb19112b2f5d48d805dae82aecfa61a9
BuffJesus <69093219+BuffJesus@users.noreply.github.com>
2025-12-06 16:32:44 -0700

    Add macros for attribute replication and OnRep implementation in GASRPG AbilitySystem

---
+++ Source/GASRPG/Public/AbilitySystem/Attributes/GASRPG_AttributeSetMacros.h
@@ -0,0 +1,14 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+// Implements the OnRep function for an attribute
+#define IMPLEMENT_ATTRIBUTE_ONREP(ClassName, AttributeName) \
+void ClassName::OnRep_##AttributeName(const FGameplayAttributeData& Old##AttributeName) const \
+{ \
+GAMEPLAYATTRIBUTE_REPNOTIFY(ClassName, AttributeName, Old##AttributeName); \
+}
+
+// Registers an attribute for replication
+#define REPLICATE_ATTRIBUTE(ClassName, AttributeName) \
+DOREPLIFETIME_CONDITION_NOTIFY(ClassName, AttributeName, COND_None, REPNOTIFY_Always)
