2c70e94466aeb50b7437d3aeefa279339b73e463
BuffJesus <69093219+BuffJesus@users.noreply.github.com>
2025-12-07 15:06:40 -0700

    Add GASRPG_EffectActor with overlap logic for attribute-based health restoration

---
+++ Source/GASRPG/Private/Actors/GASRPG_EffectActor.cpp
@@ -0,0 +1,46 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "Actors/GASRPG_EffectActor.h"
+#include "AbilitySystemComponent.h"
+#include "AbilitySystemInterface.h"
+#include "AbilitySystem/Attributes/GASRPG_AttributeSet.h"
+#include "Components/SphereComponent.h"
+
+AGASRPG_EffectActor::AGASRPG_EffectActor()
+{
+	PrimaryActorTick.bCanEverTick = false;
+	
+	Mesh = CreateDefaultSubobject<UStaticMeshComponent>(TEXT("Mesh"));
+	SetRootComponent(Mesh);
+	
+	CollisionSphere = CreateDefaultSubobject<USphereComponent>(TEXT("CollisionSphere"));
+	CollisionSphere->SetupAttachment(GetRootComponent());
+	CollisionSphere->SetSphereRadius(100.0f);
+	CollisionSphere->SetCollisionProfileName("OverlapAll");
+}
+
+void AGASRPG_EffectActor::OnOverlap(UPrimitiveComponent* OverlappedComp, AActor* OtherActor,
+	UPrimitiveComponent* OtherComp, int32 OtherBodyIndex, bool bFromSweep, const FHitResult& SweepResult)
+{
+	if (IAbilitySystemInterface* ASCInterface { Cast<IAbilitySystemInterface>(OtherActor) })
+	{
+		const UGASRPG_AttributeSet* GASRPGAttributeSet = Cast<UGASRPG_AttributeSet>(ASCInterface->GetAbilitySystemComponent()->GetAttributeSet(UGASRPG_AttributeSet::StaticClass())); 
+		UGASRPG_AttributeSet* MutableGASRPGAttributeSet = const_cast<UGASRPG_AttributeSet*>(GASRPGAttributeSet);
+		MutableGASRPGAttributeSet->SetHealth(MutableGASRPGAttributeSet->GetHealth() + 25.0f);
+	}
+}
+
+void AGASRPG_EffectActor::EndOverlap(UPrimitiveComponent* OverlappedComp, AActor* OtherActor,
+	UPrimitiveComponent* OtherComp, int32 OtherBodyIndex)
+{
+	
+}
+
+void AGASRPG_EffectActor::BeginPlay()
+{
+	Super::BeginPlay();
+	
+	CollisionSphere->OnComponentBeginOverlap.AddDynamic(this, &AGASRPG_EffectActor::OnOverlap);
+	CollisionSphere->OnComponentEndOverlap.AddDynamic(this, &AGASRPG_EffectActor::EndOverlap);
+}
---
+++ Source/GASRPG/Public/Actors/GASRPG_EffectActor.h
@@ -0,0 +1,36 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/Actor.h"
+#include "GASRPG_EffectActor.generated.h"
+
+class USphereComponent;
+class UStaticMeshComponent;
+
+UCLASS()
+class GASRPG_API AGASRPG_EffectActor : public AActor
+{
+	GENERATED_BODY()
+	
+public:
+	AGASRPG_EffectActor();
+	
+	UFUNCTION()
+	virtual void OnOverlap(UPrimitiveComponent* OverlappedComp, AActor* OtherActor, UPrimitiveComponent* OtherComp, int32 OtherBodyIndex, bool bFromSweep, const FHitResult& SweepResult);
+	
+	UFUNCTION()
+	virtual void EndOverlap(UPrimitiveComponent* OverlappedComp, AActor* OtherActor, UPrimitiveComponent* OtherComp, int32 OtherBodyIndex);
+	
+protected:
+	virtual void BeginPlay() override;
+	
+private:
+	UPROPERTY(VisibleAnywhere)
+	TObjectPtr<USphereComponent> CollisionSphere;
+	
+	UPROPERTY(VisibleAnywhere)
+	TObjectPtr<UStaticMeshComponent> Mesh;
+
+};
