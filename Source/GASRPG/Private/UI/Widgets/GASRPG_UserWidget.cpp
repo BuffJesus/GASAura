fd6dda265b80b4579152d10aaeea503064522603
BuffJesus <69093219+BuffJesus@users.noreply.github.com>
2025-12-10 20:42:51 -0700

    Log widget controller name to screen when set, update HUD and progress bar blueprints.

--- Source/GASRPG/Private/UI/Widgets/GASRPG_UserWidget.cpp
+++ Source/GASRPG/Private/UI/Widgets/GASRPG_UserWidget.cpp
@@ -7,4 +7,6 @@ void UGASRPG_UserWidget::SetWidgetController(UObject* NewWidgetController)
 {
 	WidgetController = NewWidgetController;
 	WidgetControllerSet();
+	
+	GEngine->AddOnScreenDebugMessage(-1, 5.0f, FColor::Red, FString::Printf(TEXT("Widget Controller Set: %s"), *WidgetController->GetName()));
 }
