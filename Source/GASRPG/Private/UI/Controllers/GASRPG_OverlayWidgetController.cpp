3577c82ecfd22cb08fa4ac0773aedcfa6306e8fe
BuffJesus <69093219+BuffJesus@users.noreply.github.com>
2025-12-10 20:32:21 -0700

    Log attribute values to screen during `BroadcastInitialValues` for debugging purposes.

--- Source/GASRPG/Private/UI/Controllers/GASRPG_OverlayWidgetController.cpp
+++ Source/GASRPG/Private/UI/Controllers/GASRPG_OverlayWidgetController.cpp
@@ -7,10 +7,15 @@
 void UGASRPG_OverlayWidgetController::BroadcastInitialValues()
 {
 	const UGASRPG_AttributeSet* GASRPG_AttributeSet { Cast<UGASRPG_AttributeSet>(AttributeSet) };
-	// if (GASRPG_AttributeSet == nullptr) return;
+	if (GASRPG_AttributeSet == nullptr) return;
 
 	OnHealthChanged.Broadcast(GASRPG_AttributeSet->GetHealth());
 	OnMaxHealthChanged.Broadcast(GASRPG_AttributeSet->GetMaxHealth());
 	OnManaChanged.Broadcast(GASRPG_AttributeSet->GetMana());
 	OnMaxManaChanged.Broadcast(GASRPG_AttributeSet->GetMaxMana());
+	
+	GEngine->AddOnScreenDebugMessage(-1, 5.0f, FColor::Red, FString::Printf(TEXT("Health: %f"), GASRPG_AttributeSet->GetHealth()));
+	GEngine->AddOnScreenDebugMessage(-1, 5.0f, FColor::Red, FString::Printf(TEXT("Max Health: %f"), GASRPG_AttributeSet->GetMaxHealth()));
+	GEngine->AddOnScreenDebugMessage(-1, 5.0f, FColor::Red, FString::Printf(TEXT("Mana: %f"), GASRPG_AttributeSet->GetMana()));
+	GEngine->AddOnScreenDebugMessage(-1, 5.0f, FColor::Red, FString::Printf(TEXT("Max Mana: %f"), GASRPG_AttributeSet->GetMaxMana()));
 }
