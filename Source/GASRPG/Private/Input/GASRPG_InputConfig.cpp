4feaae665f71aea803cb3ef06376ec9504960dad
BuffJesus <69093219+BuffJesus@users.noreply.github.com>
2026-01-03 16:02:53 -0700

    Add `GASRPG_InputConfig` class and functionality for mapping input actions to gameplay tags

---
+++ Source/GASRPG/Private/Input/GASRPG_InputConfig.cpp
@@ -0,0 +1,23 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "Input/GASRPG_InputConfig.h"
+
+const UInputAction* UGASRPG_InputConfig::FindAbilityInputActionForTag(const FGameplayTag& InputTag,
+	bool bLogNotFound) const
+{
+	for (const FGASRPG_InputAction& Action: AbilityInputActions)
+	{
+		if (Action.InputAction && Action.InputTag == InputTag)
+		{
+			return Action.InputAction;
+		}
+	}
+
+	if (bLogNotFound)
+	{
+		UE_LOGFMT(LogTemp, Error, "Can't find AbilityInputAction for InputTag [{InputTag}], on InputConfig [{InputConfig}]", InputTag.ToString(), GetNameSafe(this));
+	}
+
+	return nullptr;
+}
---
+++ Source/GASRPG/Public/Input/GASRPG_InputConfig.h
@@ -0,0 +1,32 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameplayTagContainer.h"
+#include "Engine/DataAsset.h"
+#include "GASRPG_InputConfig.generated.h"
+
+USTRUCT(BlueprintType)
+struct FGASRPG_InputAction
+{
+	GENERATED_BODY()
+	
+	UPROPERTY(EditDefaultsOnly)
+	const class UInputAction* InputAction { nullptr };
+	
+	UPROPERTY(EditDefaultsOnly, meta = (Categories = "GASRPG.Input"))
+	FGameplayTag InputTag { FGameplayTag() };
+};
+
+UCLASS()
+class GASRPG_API UGASRPG_InputConfig : public UDataAsset
+{
+	GENERATED_BODY()
+	
+public:
+	const UInputAction* FindAbilityInputActionForTag(const FGameplayTag& InputTag, bool bLogNotFound = false) const;
+	
+	UPROPERTY(EditDefaultsOnly, BlueprintReadOnly)
+	TArray<FGASRPG_InputAction> AbilityInputActions;
+};
