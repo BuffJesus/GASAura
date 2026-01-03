43c451320a87f7016284c9d1a2c67e23faccee74
BuffJesus <69093219+BuffJesus@users.noreply.github.com>
2026-01-03 16:20:10 -0700

    Add `UGASRPG_InputComponent` class to handle ability action bindings
    - Implemented `BindAbilityActions` template function for mapping input actions to abilities.

---
+++ Source/GASRPG/Private/Input/GASRPG_InputComponent.cpp
@@ -0,0 +1,36 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "Input/GASRPG_InputComponent.h"
+
+
+// Sets default values for this component's properties
+UGASRPG_InputComponent::UGASRPG_InputComponent()
+{
+	// Set this component to be initialized when the game starts, and to be ticked every frame.  You can turn these features
+	// off to improve performance if you don't need them.
+	PrimaryComponentTick.bCanEverTick = true;
+
+	// ...
+}
+
+
+// Called when the game starts
+void UGASRPG_InputComponent::BeginPlay()
+{
+	Super::BeginPlay();
+
+	// ...
+	
+}
+
+
+// Called every frame
+void UGASRPG_InputComponent::TickComponent(float DeltaTime, ELevelTick TickType,
+                                           FActorComponentTickFunction* ThisTickFunction)
+{
+	Super::TickComponent(DeltaTime, TickType, ThisTickFunction);
+
+	// ...
+}
+
